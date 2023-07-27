2cb6462a15d77efa9dddb564240ca05750405ee6
JoseAnselmoGarcia <jose.anselmo@me.com>
2023-07-27 00:38:16 -0400

    modified:   app.js

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ app.use("/api/users", userRouter);
 app.listen(process.env.APP_PORT, () => {
   console.log("Shgjervidor ativo na PORTA :", process.env.APP_PORT);
 });
+
+//Aqui
+// git push
+// Vai abrir conf, tira #, salva e vai abrir auth e dar pau. Quando fechar ele faz
+
+// La
+// nom stop   ou     pm2 stop app
+// git pull origin teste
+// npm start   ou    pm2 start app
